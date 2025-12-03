84551d5d4746fdd850ae52eb0903f4314abedf3b
AnveshBajjuri <bajjuriani@gmail.com>
2025-12-03 21:46:02 +0530

    last

--- SmartHabitTracker/server/config/settings.py
+++ SmartHabitTracker/server/config/settings.py
@@ -82,9 +82,14 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
